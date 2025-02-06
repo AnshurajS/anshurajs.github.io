829ef512ad4a4267903b00bcca0c4dff7aa1be6c
AnshurajS <124818768+AnshurajS@users.noreply.github.com>
2025-02-06 23:48:04 +0000

    Deploying to gh-pages from @ AnshurajS/anshurajs.github.io@5fd119d973112b7505753469693fab904545b1f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,9 +421,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-exoplanet-explorer",
+          title: 'Exoplanet Explorer',
+          description: "Interactive database of exoplanets with filtering features for its size and distance from Earth!",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
